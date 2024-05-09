5d50def12dd550325e4ccf087286413ec384f210
Алексей Русяев <RusyaevAA@outlook.com>
2024-05-09 18:18:06 +0400

    NIST tests have been performed for the sequence from the generator in the Java language

--- lab_2/nist.py
+++ lab_2/nist.py
@@ -8,17 +8,26 @@ def main() -> None:
         prog="Statistical analysis of pseudorandom sequences")
     parser.add_argument('cxx', type=str,
                         help='A file (.txt) with the sequence')
+    parser.add_argument('java', type=str,
+                        help='A file (.txt) with the sequence')
 
     args = parser.parse_args()
 
-    sequence = read_the_file(args.cxx)
-    print(f"C++ sequence: {sequence}")
-    print(f"1 test result: {frequency_bitwise_test(sequence)}")
-    print(f"2 test result: {same_consecutive_bits_test(sequence)}")
-    print(f"3 test result: {longest_sequence_of_units_in_a_block_test(sequence)}")
+    sequence_cxx = read_the_file(args.cxx)
+    print(f"C++ sequence: {sequence_cxx}")
+    print(f"1 test result: {frequency_bitwise_test(sequence_cxx)}")
+    print(f"2 test result: {same_consecutive_bits_test(sequence_cxx)}")
+    print(f"3 test result: {longest_sequence_of_units_in_a_block_test(sequence_cxx)}")
+
+    print()
+    sequence_java = read_the_file(args.java)
+    print(f"Java sequence: {sequence_java}")
+    print(f"1 test result: {frequency_bitwise_test(sequence_java)}")
+    print(f"2 test result: {same_consecutive_bits_test(sequence_java)}")
+    print(f"3 test result: {longest_sequence_of_units_in_a_block_test(sequence_java)}")
 
 
 if __name__ == "__main__":
     main()
 
-# python lab_2\\nist.py lab_2\\results\\cxx.txt
+# python lab_2\\nist.py lab_2\\results\\cxx.txt lab_2\\results\\java.txt
